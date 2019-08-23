5c83ad52c901ce71b7d05d125eaf4a96696587b4
ChenJohnny <JohnnyZsefb@ChenJohnnydeMacBook-Pro.local>
2019-08-24 01:21:32 +0800

    FirstVersion

---
+++ PTT_Crawler_Test.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.ptt.cc/bbs/stock/index.html'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+#print response.text
+articles = soup.find_all('div', 'r-ent')
+#print articles
+
+for article in articles:
+    try:
+        meta = article.find('div', 'title').find('a')
+        title = meta.getText().strip()
+        link = meta.get('href')
+        push = article.find('div', 'nrec').getText()
+        date = article.find('div', 'date').getText()
+        author = article.find('div', 'author').getText()
+    
+        print(push, title, date, author)  # result of setp-3
+    except:
+        continue
